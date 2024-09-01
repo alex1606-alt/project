3a8a9567b46b952bb039f58f4449dffb2a3c2230
Oleksandr <zubator777@gmail.com>
2024-09-01 09:28:24 +0200

    first comit

---
+++ script2.js
@@ -0,0 +1,142 @@
+
+
+
+
+
+
+
+
+
+
+//let array = [1,3,4,6,9,];
+//let suma = 0;
+
+//for(i=0; i<array.length; i++){ 
+    //if(array[i] % 2 === 0)
+
+    
+        //{suma = suma + array[i]}
+    
+//};
+    
+//const person = {
+   // name: 'Alex',
+   // age:35,
+   //lastname:"zubov"
+
+//}
+
+
+//function elza (messedg,count){
+    //for(let i =0; i < count; i++){
+        //console.log(messedg)}
+//};
+//elza( 'приветики' , 5);
+
+
+//const car = {
+    //model:"toyta",
+   // age:2007,
+    //shoymodel: function(color,engin){
+        //console.log(this.model,color,engin);
+
+    
+
+    //}
+//};
+
+
+
+
+//const car2 = {
+    //model: "reno",
+    //age: 2005,
+//};
+//car.shoymodel.apply(car2,["red","dizel"]);
+
+//console.log(array.join("."));
+
+
+
+
+
+const a = [1,4,5,7,8,8,];
+let x = 0;
+for(let i = 0; i < a.length; i++){
+if(a[i] % 2 !== 0){x = x + a[i]}};
+
+//console.log(x);
+
+
+const lengthCuzov = 2000;
+const wighCuzov = 800;
+console.log(`длина кузова = ${lengthCuzov} и ширина кузова = ${wighCuzov}`);
+
+
+
+//const fag = confirm("watsap?");
+//console.log(fag);
+//const dfg = [];
+//dfg[0] = prompt("как ваше имя");
+//dfg[1] = prompt("сколько вам лет");
+//dfg[2] = prompt("образование");
+//console.log(dfg.join("."));
+//console.log(typeof(dfg));
+
+
+
+let z = 10;
+let y = 15;
+
+z++;
+y--;
+console.log(11%3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
